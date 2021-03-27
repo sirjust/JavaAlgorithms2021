b69fff4e37d7a2b24c8e7ac71c1fb64a3e1c2ecc
Justin Moore <theoperaking@yahoo.com>
2021-03-27 16:57:25 -0400

    Add linked list implementation

--- src/com/company/BubbleSorter.java
+++ src/com/company/BubbleSorter.java
@@ -13,4 +13,15 @@ public class BubbleSorter {
         }
         return array;
     }
+
+    public int[] mySort(int[] array){
+        for(int lastUnsorted = array.length - 1; lastUnsorted > 0; lastUnsorted--) {
+            for (int i = 0; i < lastUnsorted; i++) {
+                if (array[i] > array[i + 1]) {
+                    array = swap(array, i, i + 1);
+                }
+            }
+        }
+        return array;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Employee jane = new Employee("Jane", "Jones", 123);
+        Employee john = new Employee("John", "Doe", 4567);
+        Employee mary = new Employee("Mary", "Smith", 22);
+        Employee mike = new Employee("Mike", "Wilson", 3245);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+        System.out.println("Is this list empty? " + list.isEmpty());
+        list.addToFront(jane);
+        list.addToFront(john);
+        list.addToFront(mary);
+        list.addToFront(mike);
+
+        System.out.println("Size: " + list.getSize());
+        list.printList();
+
+        list.removeFromFront();
+        System.out.println("Size: " + list.getSize());
+        list.printList();
+    }
+
+    private static void UseSorters(){
         //test();
         int[] array = new int[] {20, 35, -15, 7, 55, 1, -22};
-        // BubbleSorter sorter = new BubbleSorter();
+        BubbleSorter sorter = new BubbleSorter();
         // SelectionSorter sorter = new SelectionSorter();
         // InsertionSorter sorter = new InsertionSorter();
-        ShellSorter sorter = new ShellSorter();
-        sorter.sort(array);
+        // ShellSorter sorter = new ShellSorter();
+        // sorter.sort(array);
+        sorter.mySort(array);
         printArray(array);
     }
 
